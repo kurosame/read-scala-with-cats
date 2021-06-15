8cf7cd894ec64babf57e71021631151296306793
kurosame <kurosame1227@gmail.com>
2021-06-16 00:13:58 +0900

    add: 10.4 ~ 10.6

--- src/main/scala/sandbox/Chapter10.scala
+++ src/main/scala/sandbox/Chapter10.scala
@@ -183,4 +183,291 @@ object Chapter10 {
    * 次はデータを変換するmap関数を実装する
    */
 
+  // 10.4 Transforming Data
+  /**
+   * 現状のCheckの型エイリアスだとmapは使えない
+   *
+   * type Check[E, A] = A => Either[E, A]
+   * def map(check: Check[E, A])(func: A => B): Check[E, ???]
+   * ⇒ Check型にBが必要
+   *
+   * Checkの型エイリアスを以下に変更する
+   */
+//  type Check[E, A, B] = A => Either[E, B]
+//
+//  // この新しいCheck型により、parseIntのようにStringをIntとして解析するケースが実現できる
+//  val parseInt: Check[List[String], String, Int] = ???
+
+  /**
+   * しかし、入力型と出力型を分割すると、別の問題が発生する
+   * これまではチェックが成功すると常に入力を返してきた
+   */
+//  (this(a), that(a)) match {
+//    case And(left, right) =>
+//      (left(a), right(a)).mapN((result1, result2) => Right(a))
+//  }
+
+  /**
+   * しかし、新しいCheck型では、Either[E, B]なので、Right(a)を返すことはできない
+   * Right(result1)とRight(result2)のどちらを返すかを任意に選択する必要がある
+   */
+
+  // 10.4.1 Predicates
+  /**
+   * 今までCheckと呼んでいたものをPredicateに変更する
+   * PredicateはPredicateが成功した場合、常に入力を返す（以前のCheckと同じ）
+   */
+//  import cats.Semigroup
+//  import cats.data.Validated
+//  import cats.syntax.semigroup._
+//  import cats.syntax.apply._
+//  import cats.data.Validated._
+//
+//  sealed trait Predicate[E, A] {
+//    def and(that: Predicate[E, A]): Predicate[E, A] = And(this, that)
+//
+//    def or(that: Predicate[E, A]): Predicate[E, A] = Or(this, that)
+//
+//    def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A] =
+//      this match {
+//        case Pure(func) => func(a)
+//
+//        case And(left, right) => (left(a), right(a)).mapN((_, _) => a)
+//
+//        case Or(left, right) =>
+//          left(a) match {
+//            case Valid(_) => Valid(a)
+//            case Invalid(e1) =>
+//              right(a) match {
+//                case Valid(_)    => Valid(a)
+//                case Invalid(e2) => Invalid(e1 |+| e2)
+//              }
+//          }
+//      }
+//  }
+//  final case class And[E, A](left: Predicate[E, A], right: Predicate[E, A]) extends Predicate[E, A]
+//  final case class Or[E, A](left: Predicate[E, A], right: Predicate[E, A]) extends Predicate[E, A]
+//  final case class Pure[E, A](func: A => Validated[E, A]) extends Predicate[E, A]
+
+  // 10.4.2 Checks
+  /**
+   * 入力の変換も可能にするCheckを実装する
+   */
+//  import cats.Semigroup
+//  import cats.data.Validated
+//
+//  sealed trait Check[E, A, B] {
+//    import Check._
+//
+//    def apply(in: A)(implicit s: Semigroup[E]): Validated[E, B]
+//
+//    def map[C](f: B => C): Check[E, A, C] = Map[E, A, B, C](this, f)
+//  }
+//
+//  object Check {
+//    final case class Map[E, A, B, C](check: Check[E, A, B], func: B => C) extends Check[E, A, C] {
+//      def apply(in: A)(implicit s: Semigroup[E]): Validated[E, C] =
+//        check(in).map(func)
+//    }
+//
+//    final case class Pure[E, A](pred: Predicate[E, A]) extends Check[E, A, A] {
+//      def apply(in: A)(implicit s: Semigroup[E]): Validated[E, A] =
+//        pred(in)
+//    }
+//
+//    def apply[E, A](pred: Predicate[E, A]): Check[E, A, A] = Pure(pred)
+//  }
+
+  /**
+   * CheckのflatMapはどうなるか
+   */
+//  import cats.Semigroup
+//  import cats.data.Validated
+//
+//  sealed trait Check[E, A, B] {
+//    def apply(in: A)(implicit s: Semigroup[E]): Validated[E, B]
+//
+//    def flatMap[C](f: B => Check[E, A, C]) =
+//      FlatMap[E, A, B, C](this, f)
+//  }
+//
+//  final case class FlatMap[E, A, B, C](check: Check[E, A, B], func: B => Check[E, A, C]) extends Check[E, A, C] {
+//    def apply(a: A)(implicit s: Semigroup[E]): Validated[E, C] =
+//      check(a).withEither(_.flatMap(b => func(b)(a).toEither))
+//  }
+
+  /**
+   * 2つのチェックをチェーンするandThenを実装する
+   */
+//  import cats.Semigroup
+//  import cats.data.Validated
+//
+//  sealed trait Check[E, A, B] {
+//    def apply(in: A)(implicit s: Semigroup[E]): Validated[E, B]
+//
+//    def andThen[C](that: Check[E, B, C]): Check[E, A, C] =
+//      AndThen[E, A, B, C](this, that)
+//  }
+//
+//  final case class AndThen[E, A, B, C](check1: Check[E, A, B], check2: Check[E, B, C]) extends Check[E, A, C] {
+//    def apply(a: A)(implicit s: Semigroup[E]): Validated[E, C] =
+//      check1(a).withEither(_.flatMap(b => check2(b).toEither))
+//  }
+
+  // 10.4.3 Recap
+  /**
+   * Predicateの完全な実装は以下
+   */
+//  import cats.Semigroup
+//  import cats.data.Validated
+//  import cats.syntax.semigroup._
+//  import cats.syntax.apply._
+//  import cats.syntax.validated._
+//
+//  sealed trait Predicate[E, A] {
+//    import Predicate._
+//    import Validated._
+//
+//    def and(that: Predicate[E, A]): Predicate[E, A] = And(this, that)
+//
+//    def or(that: Predicate[E, A]): Predicate[E, A] = Or(this, that)
+//
+//    def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A] =
+//      this match {
+//        case Pure(func) => func(a)
+//
+//        case And(left, right) => (left(a), right(a)).mapN((_, _) => a)
+//
+//        case Or(left, right) =>
+//          left(a) match {
+//            case Valid(_) => Valid(a)
+//            case Invalid(e1) =>
+//              right(a) match {
+//                case Valid(_)    => Valid(a)
+//                case Invalid(e2) => Invalid(e1 |+| e2)
+//              }
+//          }
+//      }
+//  }
+//
+//  object Predicate {
+//    final case class And[E, A](left: Predicate[E, A], right: Predicate[E, A]) extends Predicate[E, A]
+//    final case class Or[E, A](left: Predicate[E, A], right: Predicate[E, A]) extends Predicate[E, A]
+//    final case class Pure[E, A](func: A => Validated[E, A]) extends Predicate[E, A]
+//
+//    def apply[E, A](f: A => Validated[E, A]): Predicate[E, A] = Pure(f)
+//
+//    def lift[E, A](err: E, fn: A => Boolean): Predicate[E, A] =
+//      Pure(a => if (fn(a)) a.valid else err.invalid)
+//  }
+
+  /**
+   * Checkの完全な実装は以下
+   */
+//  import cats.Semigroup
+//  import cats.data.Validated
+//
+//  sealed trait Check[E, A, B] {
+//    import Check._
+//
+//    def apply(in: A)(implicit s: Semigroup[E]): Validated[E, B]
+//
+//    def map[C](f: B => C): Check[E, A, C] = Map[E, A, B, C](this, f)
+//
+//    def flatMap[C](f: B => Check[E, A, C]) =
+//      FlatMap[E, A, B, C](this, f)
+//
+//    def andThen[C](next: Check[E, B, C]): Check[E, A, C] =
+//      AndThen[E, A, B, C](this, next)
+//  }
+//
+//  object Check {
+//    final case class Map[E, A, B, C](check: Check[E, A, B], func: B => C) extends Check[E, A, C] {
+//      def apply(a: A)(implicit s: Semigroup[E]): Validated[E, C] = check(a).map(func)
+//    }
+//
+//    final case class FlatMap[E, A, B, C](check: Check[E, A, B], func: B => Check[E, A, C]) extends Check[E, A, C] {
+//      def apply(a: A)(implicit s: Semigroup[E]): Validated[E, C] =
+//        check(a).withEither(_.flatMap(b => func(b)(a).toEither))
+//    }
+//
+//    final case class AndThen[E, A, B, C](check: Check[E, A, B], next: Check[E, B, C]) extends Check[E, A, C] {
+//      def apply(a: A)(implicit s: Semigroup[E]): Validated[E, C] =
+//        check(a).withEither(_.flatMap(b => next(b).toEither))
+//    }
+//
+//    final case class Pure[E, A, B](func: A => Validated[E, B]) extends Check[E, A, B] {
+//      def apply(a: A)(implicit s: Semigroup[E]): Validated[E, B] = func(a)
+//    }
+//
+//    final case class PurePredicate[E, A](pred: Predicate[E, A]) extends Check[E, A, A] {
+//      def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A] = pred(a)
+//    }
+//
+//    def apply[E, A](pred: Predicate[E, A]): Check[E, A, A] = PurePredicate(pred)
+//
+//    def apply[E, A, B](func: A => Validated[E, B]): Check[E, A, B] = Pure(func)
+//  }
+
+  /**
+   * Predicateにはモノイドがあり、Checkにはモナドがある
+   * いくつかのバリデーションチェックの例を実装する
+   * Predicateは入力のチェックがTrueの場合、入力を返す
+   * Checkは入力を変換して、返す
+   */
+//  import cats.data.{NonEmptyList, Validated}
+//  import cats.syntax.apply._
+//  import cats.syntax.validated._
+//
+//  type Errors = NonEmptyList[String]
+//
+//  def error(s: String): NonEmptyList[String] = NonEmptyList(s, Nil)
+//
+//  def longerThan(n: Int): Predicate[Errors, String] =
+//    Predicate.lift(error(s"Must be longer than $n characters"), str => str.size > n)
+//
+//  val alphanumeric: Predicate[Errors, String] =
+//    Predicate.lift(error(s"Must be all alphanumeric characters"), str => str.forall(_.isLetterOrDigit))
+//
+//  def contains(char: Char): Predicate[Errors, String] =
+//    Predicate.lift(error(s"Must contain the character $char"), str => str.contains(char))
+//
+//  def containsOnce(char: Char): Predicate[Errors, String] =
+//    Predicate.lift(error(s"Must contain the character $char only once"), str => str.filter(c => c == char).size == 1)
+//
+//  val checkUsername: Check[Errors, String, String] = Check(longerThan(3).and(alphanumeric))
+//
+//  val splitEmail: Check[Errors, String, (String, String)] =
+//    Check(_.split('@') match {
+//      case Array(name, domain) => (name, domain).validNel[String]
+//      case _                   => "Must contain a single @ character".invalidNel[(String, String)]
+//    })
+//
+//  val checkLeft: Check[Errors, String, String] = Check(longerThan(0))
+//
+//  val checkRight: Check[Errors, String, String] = Check(longerThan(3).and(contains('.')))
+//
+//  val joinEmail: Check[Errors, (String, String), String] =
+//    Check { case (l, r) =>
+//      (checkLeft(l), checkRight(r)).mapN(_ + "@" + _)
+//    }
+//
+//  val checkEmail: Check[Errors, String, String] = splitEmail.andThen(joinEmail)
+//
+//  final case class User(username: String, email: String)
+//
+//  def createUser(username: String, email: String): Validated[Errors, User] =
+//    (checkUsername(username), checkEmail(email)).mapN(User)
+
+  // 10.5 Kleislis
+  /**
+   * Predicateは本質的にA => Validated[E, A]であり、Checkはこれらの関数を基礎としたラッパーである
+   *
+   * A => Validated[E, A]はA => F[B]に抽象化できる
+   * これはflatMap関数に渡す関数の型である
+   *
+   * ...
+   */
+
+  // 10.6 Summary
 }
